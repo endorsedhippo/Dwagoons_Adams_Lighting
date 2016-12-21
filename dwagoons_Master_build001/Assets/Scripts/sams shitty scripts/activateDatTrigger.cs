4e312e56390a4d807af2295ca626927086229369
Leslie McLeod <leslie.mcleod@ad.aie.edu>
2016-12-21 10:57:31 +1030

    Teacher Adams lighting settings

---
+++ dwagoons_Master_build001/Assets/Scripts/sams shitty scripts/activateDatTrigger.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class activateDatTrigger  : MonoBehaviour 
+{
+
+
+	private Collider sphCol;
+	private InputDevice device;
+	public int playerIndex;
+	private HealthScript health;
+	private float timer;
+	public float timeBetweenAttacks;
+	public Animator animator;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		sphCol = GetComponent<SphereCollider>();
+		device = InputManager.Devices [playerIndex];
+		health = GameObject.FindGameObjectWithTag ("player2").GetComponent<HealthScript> ();
+		sphCol.enabled = false;
+
+	}
+
+
+	// Update is called once per frame
+	void Update ()
+	{
+		timer += Time.deltaTime;
+		if (device.Action2.IsPressed) 
+		{
+			animator.SetBool ("IsAttacking", true);
+
+			Timer ();
+
+		}
+		else 
+		{
+			animator.SetBool ("IsAttacking", false);
+		}
+
+	}
+
+	void Timer()
+	{
+		
+		{
+			if (timer > timeBetweenAttacks)
+			{
+				sphCol.enabled = true;
+
+				StartCoroutine (Combat ());
+				timer = 0;
+			}
+		}
+	}
+
+
+	IEnumerator Combat()
+	{
+		yield return new WaitForSeconds (1);
+		sphCol.enabled = false;
+
+
+	}
+
+	void OnTriggerEnter (Collider col)
+	{
+		if (col.gameObject.tag == "player2") 
+		{
+			
+			{
+				health.currentHealth -= 10;
+
+			}
+
+		}
+	}
+		
+}
---
+++ dwagoons_Master_build001/Assets/Scripts/sams shitty scripts/sandStormDelay.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sandStormDelay : MonoBehaviour 
+{
+	public GameObject sandStorm;
+	public GameObject sandStorm2;
+	public GameObject sandStorm3;
+	public GameObject player1;
+	public GameObject player2;
+	public int delay;
+	public int duriation; 
+	public int secondDelay;
+
+
+	void Start()
+	{
+		StartCoroutine ("SandStorm");
+	}
+
+
+	IEnumerator SandStorm ()
+	{
+		yield return new WaitForSeconds (delay);
+		sandStorm.SetActive (true);
+		yield return new WaitForSeconds (secondDelay);
+		sandStorm2.SetActive (true);
+		yield return new WaitForSeconds (secondDelay);
+		sandStorm3.SetActive (true);
+		yield return new WaitForSeconds (duriation);
+		sandStorm3.SetActive (false);
+		yield return new WaitForSeconds (secondDelay);
+		sandStorm2.SetActive (false);
+		yield return new WaitForSeconds (secondDelay);
+		sandStorm.SetActive (false);
+		yield return new WaitForSeconds (secondDelay);
+	}
+
+}
